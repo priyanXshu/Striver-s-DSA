94184280edfb30be8adb828d74d60fabc585f4dd
Priyanshu Singh <80763916+priyanXshu@users.noreply.github.com>
2023-08-21 23:12:19 +0530

    Update Better-sol.cpp

--- Arrays/Set Matrix Zeroes/Better-sol.cpp
+++ Arrays/Set Matrix Zeroes/Better-sol.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    
-    void markRow(int i, vector<vector<int>>& matrix){
-        for(int j=0; j<matrix[0].size(); j++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = -1;
-            }
-        }
-    }
-    void markCol(int j, vector<vector<int>>& matrix){
-        for(int i=0; i<matrix.size(); i++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = -1;
-            }
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    markRow(i, matrix);
-                    markCol(j, matrix);
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(matrix[i][j] == -1){
+                if(row[i] == 1 || col[j] == 1){
                     matrix[i][j] = 0;
                 }
             }
         }
-        // return matrix;
     }
 };
