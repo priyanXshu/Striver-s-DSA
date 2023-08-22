85e5795841c8234abb4d6ba9e9f648720b2d6701
Priyanshu Singh <80763916+priyanXshu@users.noreply.github.com>
2023-08-22 08:07:22 +0530

    Update Optimal-sol.cpp

--- Arrays/Set Matrix Zeroes/Optimal-sol.cpp
+++ Arrays/Set Matrix Zeroes/Optimal-sol.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    
-    void markRow(int i, vector<vector<int>>& matrix){
-        for(int j=0; j<matrix[0].size(); j++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = -1;
-            }
-        }
-    }
-    void markCol(int j, vector<vector<int>>& matrix){
-        for(int i=0; i<matrix.size(); i++){
-            if(matrix[i][j] != 0){
-                matrix[i][j] = -1;
-            }
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
+        int col0 = 1;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    markRow(i, matrix);
-                    markCol(j, matrix);
+                    matrix[i][0] = 0;
+                    if(j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
                 }
             }
         }
-        
-        for(int i=0; i<n; i++){
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
             for(int j=0; j<m; j++){
-                if(matrix[i][j] == -1){
-                    matrix[i][j] = 0;
-                }
+                matrix[0][j] = 0;
             }
         }
-        // return matrix;
+        if(col0 == 0){
+            for(int i=0; i<n; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
