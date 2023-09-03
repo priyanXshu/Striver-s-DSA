764f88fd71b8419f807aafdda06dafd03f47c5db
Priyanshu Singh <80763916+priyanXshu@users.noreply.github.com>
2023-09-03 10:54:13 +0530

    Add files via upload

---
+++ Arrays/22. Longest Subarray Zero Sum/better sol.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int LongestSubsetWithZeroSum(vector < int > arr) {
+  int maxLen = 0;
+  int sum = 0;
+  unordered_map<int, int> mp;
+  for(int i=0; i<arr.size(); i++){
+    sum += arr[i];
+    if(sum == 0){
+      maxLen = i + 1;
+    }
+    else if(mp.find(sum) != mp.end()){
+      maxLen = max(maxLen, i-mp[sum]);
+    }
+    else{
+      mp[sum] = i;
+    }
+  }
+  return maxLen;
+}
