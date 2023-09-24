b3ece0b9cf24a7a774d52464eb65458e0be72b56
Priyanshu Singh <80763916+priyanXshu@users.noreply.github.com>
2023-09-24 20:59:24 +0530

    Add files via upload

---
+++ Linked List/08. Linked List Cycle/brute-force.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> set;
+        while(head){
+            if(set.find(head) != set.end()) return true;
+            set.insert(head);
+            head = head->next;
+        }
+        return false;
+    }
+};
