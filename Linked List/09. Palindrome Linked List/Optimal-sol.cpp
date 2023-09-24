341cd45cda459876f2cfa2fb7f8e150186a287b7
Priyanshu Singh <80763916+priyanXshu@users.noreply.github.com>
2023-09-24 21:06:56 +0530

    Add files via upload

---
+++ Linked List/09. Palindrome Linked List/Optimal-sol.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getMid(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    ListNode* reverse(ListNode* head){
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while(curr){
+            ListNode* forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head->next == NULL)  return true;
+
+        ListNode* middle = getMid(head);
+        ListNode* temp = middle->next;
+        middle->next = reverse(temp);
+
+        ListNode* head1 = head;
+        ListNode* head2 = middle->next;
+
+        while(head2){
+            if(head1->val != head2->val)  
+                return false;
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        temp = middle->next;
+        middle->next = reverse(temp);
+        return true;
+    }
+};
